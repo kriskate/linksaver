dc0366c5e4cd125febcc03882046546fbc3ae8ed
kristi <kristi.stanescu@gmail.com>
2016-08-26 13:47:17 +0100

    added material-ui

--- src/index.js
+++ src/index.js
@@ -13,9 +13,31 @@ import './styles/main.scss';
 import { configureStore } from './store/configureStore';
 import { Root } from './containers/Root';
 
+// Needed for material-ui
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+  // Needed for onTouchTap
+  // http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// RUN
+
+injectTapEventPlugin();
+
 const store = configureStore();
 
+// material-ui config
+import {deepOrange500} from 'material-ui/styles/colors';
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+
 ReactDOM.render(
-  <Root store={store} />,
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Root store={store} />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
